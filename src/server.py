c20bb8a6a66249d4e1b67a2cd0ea00cb3315c36e
Paul Ambrose <pambrose@mac.com>
2022-04-28 17:50:32 -0700

    Initial commit

---
+++ src/server.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python3
+
+
+import argparse
+import logging
+import os
+
+from flask import Flask
+from flask import Response
+from flask import abort
+from flask import jsonify
+from flask_httpauth import HTTPBasicAuth
+
+from database_students import fetch_all_students
+from utils import setup_logging
+
+PORT = 'port'
+LOG_LEVEL = 'loglevel'
+
+logger = logging.getLogger(__name__)
+
+auth = HTTPBasicAuth()
+http = Flask(__name__)
+
+
+@http.route('/')
+def root():
+    return Response('This is a really cool app', mimetype='text/plain')
+
+
+@http.route('/students-json', methods=['GET'])
+def students_json():
+    students = fetch_all_students()
+    if len(students) == 0:
+        abort(404)
+    s = map(lambda x: x.__dict__, students)
+    return jsonify({'students': list(s)})
+
+
+@http.route('/students-html', methods=['GET'])
+def students_html():
+    students = fetch_all_students()
+    if len(students) == 0:
+        abort(404)
+
+    names = ''
+    for student in students:
+        names += f'<tr> <td> {student.email} </td> <td> {student.grad_year} </td> </tr>'
+
+    text = f'''
+    <html>
+        <head>
+        </head>
+        <body>
+            <table>
+            {names}
+            </table>
+        </body>
+    </html>
+    '''
+    return Response(text, mimetype='text/html')
+
+
+def main():
+    # Parse CLI args
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-p', '--port', dest=PORT, default=8080, help='HTTP port [8080]')
+    parser.add_argument('-v', '--verbose', dest=LOG_LEVEL, default=logging.INFO, action='store_const',
+                        const=logging.DEBUG, help='Enable debugging info')
+    args = vars(parser.parse_args())
+
+    # Setup logging
+    setup_logging(level=args[LOG_LEVEL])
+
+    port = int(os.environ.get('PORT', args[PORT]))
+    logger.info(f"Starting server listening on port {port}")
+
+    http.run(debug=False, port=port, host='0.0.0.0')
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/utils.py
@@ -0,0 +1,43 @@
+import logging
+import platform
+import sys
+import time
+
+
+def add_http_prefix(url):
+    return url if url.startswith('http://') or url.startswith('https://') else 'http://' + url
+
+
+def is_raspi():
+    return platform.system() == 'Linux'
+
+
+def is_windows():
+    return sys.platform == 'win32'
+
+
+def is_python3():
+    return sys.version_info[0] >= 3
+
+
+def current_time_millis():
+    return int(round(time.time() * 1000))
+
+
+def wait_for_keyboard_interrupt():
+    try:
+        while True:
+            time.sleep(60)
+    except KeyboardInterrupt:
+        pass
+
+
+def setup_logging(filename=None,
+                  filemode='a',
+                  stream=sys.stderr,
+                  level=logging.INFO,
+                  format2='%(asctime)s %(name)-10s %(funcName)-10s():%(lineno)i: %(levelname)-6s %(message)s'):
+    if filename:
+        logging.basicConfig(filename=filename, filemode=filemode, level=level, format=format2)
+    else:
+        logging.basicConfig(stream=stream, level=level, format=format2)
